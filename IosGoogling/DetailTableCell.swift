5125bf085d81c129f739dfb83bb9dbf3cab07d60
Osama aka bullhead <m.osamabinomar@gmail.com>
2017-09-24 13:12:53 +0500

    created detail for home screen

---
+++ IosGoogling/DetailTableCell.swift
@@ -0,0 +1,14 @@
+//
+//  DetailTableCell.swift
+//  IosGoogling
+//
+//  Created by Osama Bin Omar on 9/24/17.
+//  Copyright © 2017 Osama Bin Omar. All rights reserved.
+//
+
+import UIKit
+class DetailTableCell: UITableViewCell {
+    
+    @IBOutlet weak var detailTitle: UILabel!
+    @IBOutlet weak var detailDescription: UILabel!
+}
---
+++ IosGoogling/HomeDetailController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeDetailController.swift
+//  IosGoogling
+//
+//  Created by Osama Bin Omar on 9/24/17.
+//  Copyright © 2017 Osama Bin Omar. All rights reserved.
+//
+
+import UIKit
+class HomeDetailController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
