b8d171ff41a1e8e635db53238472daa19cc42d34
Osama aka bullhead <m.osamabinomar@gmail.com>
2017-09-24 15:02:56 +0500

    loading in second view success

--- IosGoogling/HomeDetailController.swift
+++ IosGoogling/HomeDetailController.swift
@@ -14,9 +14,32 @@ class HomeDetailController: UITableViewController {
         super.viewDidLoad()
         DataHandler.loadDetailData(title: detailTitle!) { response in
             self.data=response
+            self.tableView.reloadData()
         }
     }
     public func setTitle(title:String){
         self.detailTitle=title;
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellId="HomeDetailCell"
+        guard let cell=tableView.dequeueReusableCell(withIdentifier: cellId,for:indexPath) as? DetailTableCell else{
+            fatalError("Should be of type Home")
+        }
+        if data.count==0 {
+            return cell
+        }
+        let name=Array(data.keys)[indexPath.item]
+        cell.detailTitle.text=name
+        cell.detailDescription.text=data[name]
+        tableView.rowHeight=70
+        return cell
+    
     }
 }
