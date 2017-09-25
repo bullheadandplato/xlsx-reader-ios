82ef6100a42c4dadf6816bcca98dd8e129f3ae0b
Osama aka bullhead <m.osamabinomar@gmail.com>
2017-09-25 16:12:34 +0500

    show search and hide search

--- AdvisorApp/ViewController.swift
+++ AdvisorApp/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UISearchBarDelegate {
+    @IBOutlet weak var searchBar: UISearchBar!
     var tableViewController:HomeTableViewController?;
     var homeData=[String()]
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate=self
         //  show loading indicator
         let alert: UIAlertView = UIAlertView(title: "Loading Data", message: "Please wait...", delegate: nil, cancelButtonTitle: nil);
         
@@ -37,6 +39,31 @@ class ViewController: UIViewController {
 
             }
         })
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    
+        if homeData.count > 0
+        {
+            if(searchText.characters.count>0){
+                searchBar.showsCancelButton=true
+            let filtered = homeData.filter{$0.lowercased().contains(searchText.lowercased()) != false}
+            tableViewController?.setData(values: filtered)
+            tableViewController?.tableView.reloadData()
+            }else{
+                tableViewController?.setData(values: homeData)
+                tableViewController?.tableView.reloadData()
+            }
+            
+        }
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton=false
+        searchBar.text=""
+        searchBar.endEditing(true)
+        searchBar.perform(#selector(self.resignFirstResponder), with: nil, afterDelay: 0.1)
+        tableViewController?.setData(values: homeData)
+        tableViewController?.tableView.reloadData()
     }
     @IBOutlet weak var containerView: UIView!
     
